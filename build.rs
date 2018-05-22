b66d7ba1a2076759c2a9ae2a8bf160d0cdf3ae79
Martin Bertsche <martin72216@googlemail.com>
2018-05-22 23:50:59 +0200

    removed all external crates

--- build.rs
+++ build.rs
@@ -1,5 +1,32 @@
 
-fn main() -> () {
+use std::fs;
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+
+    let link_files = vec![
+        "link/link.x",
+        "link/memory.x",
+        "link/device.x",
+    ];
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let out_dir = PathBuf::from(out_dir);
+
+    for file_path in link_files {
+        {
+            let file_path = PathBuf::from(file_path);
+            let file_name = file_path.file_name().unwrap();
+            let out_path = PathBuf::from(out_dir.join(file_name));
+
+            fs::copy(&file_path, &out_path).unwrap();
+        }
+
+        println!("cargo:rerun-if-changed={}", file_path);
+    }
+
+    println!("cargo:rerun-if-changed=build.rs");
 
     ()
 }
--- src/bin/stm32_rcc_test/main.rs
+++ src/bin/stm32_rcc_test/main.rs
@@ -2,23 +2,9 @@
 #![no_main]
 #![no_std]
 
-extern crate stm32f429;
-
-#[macro_use]
-extern crate cortex_m_rt;
-extern crate cortex_m_semihosting;
-extern crate panic_semihosting;
-
 pub mod exceptions;
 
-use core::fmt::Write;
-use cortex_m_semihosting::hio;
-
-entry!(main);
-
 fn main() -> ! {
-    let mut stdout = hio::hstdout().unwrap();
-    writeln!(stdout, "Hello, world!").unwrap();
 
-    loop {}
+    loop { }
 }
